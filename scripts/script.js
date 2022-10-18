d1594adc14ea84ce367cd8337fde6649305f0cf9
Nain <justin@hnain.dev>
2022-10-18 14:42:44 -0700

    Specify algorithm for grid creation and begin code

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,9 @@ const container = document.querySelector("div.container");
 const gridHeight = 4;
 const gridWidth = 4;
 
+for (let i = 1; i <= gridHeight; i++) {
+    // append sub containers with flexbox box model according to gridHeight
+    const subContainer = document.createElement("div");
+    subContainer.classList.toggle("vertical-container");
+    // loop through subs & add grid boxes to them in accordance with gridWidth
+}
