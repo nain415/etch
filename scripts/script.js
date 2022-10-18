2212389716a4b8e74b0929ddb1b2f017f8e6863c
Nain <justin@hnain.dev>
2022-10-18 14:50:18 -0700

    Set descriptive IDs on all generated divs

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,13 @@ for (let i = 1; i <= gridHeight; i++) {
     // append sub containers with flexbox box model according to gridHeight
     const gridContainer = document.createElement("div");
     gridContainer.classList.add("vertical");
+    gridContainer.setAttribute("id", `vertical-${i}`)
     container.appendChild(gridContainer);
     // loop through subs & add grid boxes to them in accordance with gridWidth
     for (let j = 1; j <= gridWidth; j++) {
         const gridBox = document.createElement("div");
         gridBox.classList.add("box");
+        gridBox.setAttribute("id",`box-(${i},${j})`);
         gridContainer.appendChild(gridBox);
     }
 }
