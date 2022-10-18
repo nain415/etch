bb237a3c9e5901f8dcff181d6aeb427aacb3fe47
Nain <justin@hnain.dev>
2022-10-18 14:50:53 -0700

    Set hardcoded vals to uppercase

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 const container = document.querySelector("div.container");
 
-const gridHeight = 4;
-const gridWidth = 4;
+const GRIDHEIGHT = 4;
+const GRIDWIDTH = 4;
 
-for (let i = 1; i <= gridHeight; i++) {
-    // append sub containers with flexbox box model according to gridHeight
+for (let i = 1; i <= GRIDHEIGHT; i++) {
+    // append sub containers with flexbox box model according to GRIDHEIGHT
     const gridContainer = document.createElement("div");
     gridContainer.classList.add("vertical");
     gridContainer.setAttribute("id", `vertical-${i}`)
     container.appendChild(gridContainer);
-    // loop through subs & add grid boxes to them in accordance with gridWidth
-    for (let j = 1; j <= gridWidth; j++) {
+    // loop through subs & add grid boxes to them in accordance with GRIDWIDTH
+    for (let j = 1; j <= GRIDWIDTH; j++) {
         const gridBox = document.createElement("div");
         gridBox.classList.add("box");
         gridBox.setAttribute("id",`box-(${i},${j})`);
