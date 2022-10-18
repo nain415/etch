8c305a018506d082df76173835f5add108a1a8df
Nain <justin@hnain.dev>
2022-10-18 16:32:32 -0700

    Move out createGrid function

--- scripts/script.js
+++ scripts/script.js
@@ -7,22 +7,13 @@ function main(h=GRIDHEIGHT, w=GRIDWIDTH) {
     containerHeight = Number(containerHeight.substring(0,containerHeight.length-2));
     let verticalHeight = containerHeight / GRIDHEIGHT;
 
-    function createGrid() {
-        for (let i = 1; i <= GRIDHEIGHT; i++) {
-            // append sub containers with flexbox box model according to GRIDHEIGHT
-            const gridContainer = document.createElement("div");
-            gridContainer.classList.add("vertical");
-            gridContainer.setAttribute("id", `vertical-${i}`);
-            container.appendChild(gridContainer);
-            // loop through subs & add grid boxes to them in accordance with GRIDWIDTH
-            for (let j = 1; j <= GRIDWIDTH; j++) {
-                const gridBox = document.createElement("div");
-                gridBox.classList.add("box");
-                gridBox.setAttribute("id",`box-(${i},${j})`);
-                gridContainer.appendChild(gridBox);
-            };
-        };
-    }
+    // loop through subs & add grid boxes to them in accordance with GRIDWIDTH
+    for (let j = 1; j <= GRIDWIDTH; j++) {
+    const gridBox = document.createElement("div");
+    gridBox.classList.add("box");
+    gridBox.setAttribute("id",`box-(${i},${j})`);
+    gridContainer.appendChild(gridBox);
+    };
 
     function alterVertHeights() {
         let verticalList = document.querySelectorAll(".vertical");
@@ -47,12 +38,9 @@ function main(h=GRIDHEIGHT, w=GRIDWIDTH) {
         });
     }
 
-    function setHtWdth(size) {
-        GRIDHEIGHT = GRIDWIDTH = size;
-    }
-
     function clearGrid() {
-        return null;
+        boxes = document.querySelectorAll(".box");
+        boxes.forEach(box => box.remove());
     }
 
     createGrid();
@@ -61,4 +49,15 @@ function main(h=GRIDHEIGHT, w=GRIDWIDTH) {
     attachSizeListener();
 }
 
+function createGrid() {
+    for (let i = 1; i <= GRIDHEIGHT; i++) {
+        // append sub containers with flexbox box model according to GRIDHEIGHT
+        const gridContainer = document.createElement("div");
+        gridContainer.classList.add("vertical");
+        gridContainer.setAttribute("id", `vertical-${i}`);
+        container.appendChild(gridContainer);
+    }
+}
+
+createGrid();
 main();
